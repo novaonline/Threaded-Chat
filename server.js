64671cabbdee3b6e633ddd08fe64fd2096fcd26a
manny <eman_the_man@hotmail.com>
2015-02-11 00:03:16 -0600

    d

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var port = process.env.PORT || 5000;
 process.env.PWD = process.cwd();
 
 app.use("/",express.static(process.env.PWD + '/public'))
-
+/*
 app.get('/', function (req, res) {
 	res.sendfile('public/index.html'); //our link to the index.html aka our views
-});
+}); */
 
 /*
 This means that the server can push messages to clients. Whenever you write a chat message, 
